2f79c09e57dcf3e52a60348f2727b915238df697
leahnp613 <leahnagarpowers@gmail.com>
2023-10-11 11:37:53 -0400

    working on breast_exams models

--- models/breast_exams.py
+++ models/breast_exams.py
@@ -1,1 +1,29 @@
-Test Test
+
+
+from datetime import datetime
+from xmlrpc.client import Boolean
+
+
+class BreastExam_Create(BaseModel):
+    date: datetime
+    concerns: str
+    lumps: str
+    pain: Boolean
+    discoloration: str
+    discharge: str
+    size: str
+    shape: str
+    lymph_nodes: str
+    areola_changes: str
+    tissue_consistency: str
+
+
+class BreastExam_History(BaseModel):
+    
+
+class BreastExam_Update(BaseModel):
+    date: datetime
+    time: datetime
+
+
+class BreastExam_Delete():
